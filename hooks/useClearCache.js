f5d2701857603d7d2cc35985dbcacefeb4d74c55
Jinwook Kim <redwoodnatlpark@gmail.com>
2023-01-17 08:40:47 +0900

    [Remove] 사용하지 않게 된 파일들 제거.

--- hooks/useClearCache.js
+++ hooks/useClearCache.js
@@ -2,16 +2,17 @@ import FileSystem from "react-native-fs";
 import useErrorHandler from "./useErrorHandler";
 
 const useClearCache = () => {
-  const errorHandler = useErrorHandler();
-  return async paths => {
-    try {
-      for (const path of paths) {
-        await FileSystem.unlink(path);
-      }
-    } catch (error) {
-      errorHandler("CACHE_CLEAR_ERROR", error);
-    }
-  };
+    const errorHandler = useErrorHandler();
+    return async paths => {
+        try {
+            for (const path of paths) {
+                await FileSystem.unlink(path);
+            }
+            console.log("Cache cleared.");
+        } catch (error) {
+            errorHandler("CACHE_CLEAR_ERROR", error);
+        }
+    };
 };
 
 export default useClearCache;
--- hooks/useDeleteCache.js
+++ hooks/useDeleteCache.js
@@ -2,14 +2,15 @@ import FileSystem from "react-native-fs";
 import useErrorHandler from "./useErrorHandler";
 
 const useDeleteCache = () => {
-  const errorHandler = useErrorHandler();
-  return async path => {
-    try {
-      await FileSystem.unlink(path);
-    } catch (error) {
-      errorHandler("CACHE_DELETION_ERROR", error);
-    }
-  };
+    const errorHandler = useErrorHandler();
+    return async path => {
+        try {
+            await FileSystem.unlink(path);
+            console.log("Cache deleted.");
+        } catch (error) {
+            errorHandler("CACHE_DELETION_ERROR", error);
+        }
+    };
 };
 
 export default useDeleteCache;
